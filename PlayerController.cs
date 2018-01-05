562226878d5a1184816116e0b2ddc5342f933f70
JMAR <34893438+JMAR-DNY@users.noreply.github.com>
2018-01-05 16:21:05 -0500

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    private int count;
+    private Rigidbody rb;
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = "";
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count+ 1;
+            SetCountText();
+        }
+
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12){
+            winText.text = "You WIN!";
+        }
+    }
+
+
+
+}
+
+
